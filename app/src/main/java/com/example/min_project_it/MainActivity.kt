be8185caeeffcd92680a3c024655b857a08b6d76
ishant <ishantkuwt@gmail.com>
2023-10-18 00:49:56 +0530

    commit 1 done

--- app/src/main/java/com/example/min_project_it/MainActivity.kt
+++ app/src/main/java/com/example/min_project_it/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.min_project_it
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.min_project_it.Fragments.Fragment1
+import com.example.min_project_it.Fragments.Fragment2
+import com.example.min_project_it.Fragments.Fragment3
 import com.example.min_project_it.databinding.ActivityMainBinding
 import com.example.min_project_it.databinding.ActivityOtpBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -17,9 +20,32 @@ class MainActivity : AppCompatActivity() {
 
         firebaseAuth=FirebaseAuth.getInstance()
         checkUser()
-        binding.logoutBtn.setOnClickListener {
-            firebaseAuth.signOut()
-            checkUser()
+//        binding.logoutBtn.setOnClickListener {
+//            firebaseAuth.signOut()
+//            checkUser()
+//        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    showHomeFragment()
+                    true
+                }
+                R.id.profile -> {
+                    showAccountFragment()
+                    true
+                }
+                R.id.settings -> {
+                    showSettingsFragment()
+                    true
+                }
+                R.id.logout -> {
+                    firebaseAuth.signOut()
+                    checkUser()
+                    true
+                }
+                else -> false
+            }
         }
 
 
@@ -37,5 +63,31 @@ class MainActivity : AppCompatActivity() {
             val phone=firebaseUser.phoneNumber
 
         }
+    }
+
+    private fun showHomeFragment() {
+        // Set the toolbar title
+        val fragment = Fragment1()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.frameLayout.id, fragment, "HomeFragment")
+        fragmentTransaction.commit()
+    }
+
+    // Function to show the AccountFragment
+    private fun showAccountFragment() {
+        // Set the toolbar title
+        val fragment = Fragment2()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.frameLayout.id, fragment, "AccountFragment")
+        fragmentTransaction.commit()
+    }
+
+    // Function to show the SettingsFragment
+    private fun showSettingsFragment() {
+        // Set the toolbar title
+        val fragment = Fragment3()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.frameLayout.id, fragment, "SettingsFragment")
+        fragmentTransaction.commit()
     }
 }
